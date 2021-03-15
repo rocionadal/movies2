5139c09c2139fdb200ea18ee3957e5f1cba7dc18
rocionadal <rocioonadal@gmail.com>
2021-03-15 16:49:49 +0100

    simple filter method

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,6 +3,33 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
+const renderMovies = (filter = '') => {
+  const movieList = document.getElementById('movie-list');
+
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
+  } else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+
+  const filteredMovies = !filter 
+    ? movies 
+    : movies.filter(movie => movie.info.title.includes(filter));
+
+  filteredMovies.forEach(movie => {
+    const movieElement = document.createElement('li');
+    let text = movie.info.title + ' - ';
+    for (const key in movie.info) {
+      if (key !== 'title') {
+        text = text + `${key}: ${movie.info[key]}`;
+      }
+    }
+    movieElement.textContent = text;
+    movieList.append(movieElement);
+  });
+};
+
 const addMovieHandler = () => {
   const title = document.getElementById('title').value;
   const extraName = document.getElementById('extra-name').value;
@@ -21,6 +48,13 @@ const addMovieHandler = () => {
   };
 
   movies.push(newMovie);
+  renderMovies();
+};
+
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
 };
 
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
